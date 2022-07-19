34caf421411fdb8efcc9b5a1a16db19bcb25ba86
Tom Dickelman <tsdickelman@gmail.com>
2022-07-19 09:48:55 -0500

    Character Converter

---
+++ Unit 5/converter.cpp
@@ -0,0 +1,64 @@
+//**********************************************************
+// Program: Telephone Digits
+// This is an example of a sentinel-controlled loop. This
+// program converts uppercase letters to their corresponding
+// telephone digits.
+//**********************************************************
+#include <iostream>
+
+using namespace std;   
+
+int main()
+{   
+  char letter;
+  int digit, num;
+
+  cout << "Program to convert letters to their "
+  << "corresponding telephone digits."
+  << endl;   
+
+  cout << "To stop the program enter #." << endl;
+
+  cout << "Enter a letter: ";
+  cin >> letter;
+  cout << endl;
+
+  while (letter != '#')
+  {
+    cout << "Letter: " << letter;
+    cout << ", Corresponding telephone digit: ";
+
+    num = static_cast<int>(letter)
+    - static_cast<int>('A');
+    if (num > 25)
+    num = num - 32;
+
+  if ((0 <= num && num <= 25)) // || (32 <= num && num <= 57))   
+  {
+    digit = (num / 3) + 2;   
+
+  if (((num / 3 == 6 ) || (num / 3 == 7))
+    && (num % 3 == 0))
+    digit = digit - 1;
+
+  if (digit > 9)
+    digit = 9;   
+
+  cout << digit << endl;   
+}
+  else   
+  cout << "Invalid input." << endl;
+
+  cout << "\nEnter another uppercase "
+  << "letter to find its corresponding "
+  << "telephone digit." << endl;   
+  cout << "To stop the program enter #."
+  << endl;
+
+  cout << "Enter a letter: ";
+  cin >> letter;
+  cout << endl;   
+}//end while
+
+return 0;
+}
